7932b5b898960e7c07964cee9b5328b33e16820c
ingegnerJP <64368310+ingegnerJP@users.noreply.github.com>
2020-04-26 17:39:12 +0200

    schizzo di menù iniziale 26/04/2020

---
+++ Progetto PHP/Menu.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Sasso carta forbice</title> 
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" media="all" type="text/css" href="menu.css">	
+	</head>
+	<body>
+	<div id="parent">
+		<div class="container" style="text-align:center;">
+			<span class="maintext">SASSO CARTA FORBICE</span>
+			<span class="buttons">
+				<a href="Game.php" class="playbutton" style="text-decoration:none;">GIOCA</a><br>
+				<a href="Rank.php" class="rankbutton" style="text-decoration:none;">CLASSIFICA</a><br>
+				<a href="Rules.php" class="infobutton" style="text-decoration:none;">REGOLE</a><br><br>
+			</span>
+		</div>
+	</div>
+	
+	
+	
+	
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</body>
+</html>
