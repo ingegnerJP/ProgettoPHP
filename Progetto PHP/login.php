a3d736342057b0f2714bf5aeb2c237363e18cf4b
B0scar <oscargalacsi2@gmail.com>
2020-05-28 18:49:50 +0200

    2dqw
    wqewq

--- Progetto PHP/login.php
+++ Progetto PHP/login.php
@@ -1,22 +1,69 @@
-<?php
+<?php 
 
-  if(isset($_POST["user"]) && isset($_POST["pass"])){
-    
-  }
+session_start();
 
-  ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <input required type="text" name="user" value="">
-    <input required type="text" name="pass" value="">
-    <input type="button" name="inv" value="invio">
-    <div class=""> se non sei registrato premi
-      <a href="register.php">qui</a>
-    </div>
-  </body>
+$errore = "";
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+		if(isset($_POST["username"]) && isset($_POST["password"])){
+			$username = $_POST["username"];
+			$password = $_POST["password"];
+			if(($username == "a") && ($password == "a")){
+				$_SESSION["loggato"] = true;
+				header("location: Menu.php");
+			}else{
+				$errore = "I dati inseriti non sono corretti";
+			}
+		}
+}
+
+?>
+<!doctype html>
+<html>
+	<head>
+		<title>Login</title> 
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" media="all" type="text/css" href="login.css">	
+		
+	</head>
+	<body> 
+		
+		<div class="centro form">
+		<form action="Login.php" method="POST">
+		  <div class="form-group">
+			<label for="nome" class=" col-form-label username">Username</label>
+			<div class="col-sm-10">  
+			  <input type="text" class="form-control" name="username" placeholder="" value="" style="width:250px; font-family:dark_poestry; font-size:10px;">
+			</div>
+		  </div>
+		  
+		  <div class="form-group">
+			<label for="cognome" class=" col-form-label password">Password</label>
+			<div class="col-sm-10">
+			  <input type="password" class="form-control" name="password" placeholder="" value="" style="width:250px; font-family:dark_poestry; font-size:10px;">
+			</div>
+		  </div>
+		  
+		  <div class="form-group">
+			  <button type="submit" class="loginbutton"  name="login">Login</button>
+		  </div>
+		  <div class="form-group text">
+			  Non sei ancora registrato? Clicca <a href="Register.php" class="register">qui</a>
+		  </div>
+		</form>
+		</div>	
+		
+		<!--<div class="container" style="margin-top:50px; color:white;">
+		<?php
+			if(isset($errore)){
+				echo $errore;
+			}
+		?>
+		</div>-->
+		
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</body>
 </html>
--- Progetto PHP/register.php
+++ Progetto PHP/register.php
@@ -1,40 +1,57 @@
-<?php
+<?php 
+
 session_start();
-require_once "SleekDB.php";
-$dataDir = "mydb";
-$newsStore = \SleekDB\SleekDB::store('news', $dataDir);
-
-if (isset($_POST["t"])) {
-  $newsInsertable = [
-      "u" =>htmlspecialchars($_POST["user"]) ,
-      "p" => htmlspecialchars($_POST["pass"]),
-  ];
-
-  $results = $newsStore->insert( $newsInsertable );
-}
- ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-pagina protetta per la modifica dei dati
-<form class="" action="" method="post">
-  titolo:<input type="text" name="user" value=""><br>
-  descrizione:<input type="text" name="pass" value=""><br>
-  <input type="submit" name="" value="salva">
 
 
-      <?php
-      $news = $newsStore->fetch();
-       foreach ($news as $key => $value) {
-         echo " ".$value["u"]."".$value["p"]." ";
-       }
-       ?>
-
-</form>
-
-  </body>
+?>
+<!doctype html>
+<html>
+	<head>
+		<title>Registrati</title> 
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" media="all" type="text/css" href="register.css">	
+		
+	</head>
+	<body> 
+		
+		<div class="centro form">
+		<form action="Register.php" method="POST">
+		
+		
+		  <div class="form-group">
+			<label for="email" class="email">Indirizzo email</label>
+			<div class="col-sm-10"> 
+				<input type="email" class="form-control" name="email" style="width:250px; font-family:dark_poestry; font-size:10px; margin-bottom:-8px;">
+			</div>
+		  </div>
+		  
+		  <div class="form-group">
+			<label for="nome" class=" col-form-label username">Username</label>
+			<div class="col-sm-10">  
+			  <input type="text" class="form-control" name="username" placeholder="" value="" style="width:250px; font-family:dark_poestry; font-size:10px;">
+			</div>
+		  </div>
+		  
+		  <div class="form-group">
+			<label for="cognome" class=" col-form-label password">Password</label>
+			<div class="col-sm-10">
+			  <input type="password" class="form-control" name="password" placeholder="" value="" style="width:250px; font-family:dark_poestry; font-size:10px;">
+			</div>
+		  </div>
+		  
+		  <div class="form-group">
+			  <button type="submit" class="loginbutton"  name="login">Registrati</button>
+		  </div>
+		  <div class="form-group text">
+			   Hai gia' un'account? Clicca <a href="Login.php" class="register">qui</a>
+		  </div>
+		</form>
+		</div>	
+		
+		
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</body>
 </html>
